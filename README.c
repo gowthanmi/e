1a25de56bb1c8e9643604b4173d82c66382ebd0f
gowthanmi <gowthamivelusamy4@gmail.com>
2016-07-16 15:14:47 +0530

    finish

---
+++ README.c
@@ -0,0 +1,14 @@
+# e
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[15];
+char *rev;
+printf("enter the string");
+scanf("%s",str);
+rev=strrev(str);
+printf("the reversed string is": %s,rev);
+getch();
+}
